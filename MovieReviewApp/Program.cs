ddfe0496ef202f83b9dba3166b7c51497a5c0885
Jared Browne <statick@msn.com>
2024-09-10 18:37:42 -0700

    file upload finished

--- MovieReviewApp/Program.cs
+++ MovieReviewApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using MovieReviewApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,33 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddHttpClient(); // Register HttpClient
 
-var app = builder.Build();
-
-// Middleware to redirect HTTPS to HTTP
-app.Use(async (context, next) =>
+// Configure file upload size limit (10GB)
+builder.Services.Configure<FormOptions>(options =>
 {
-    if (context.Request.IsHttps)
-    {
-        var httpUrl = $"http://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}";
-        context.Response.Redirect(httpUrl);
-    }
-    else
-    {
-        await next();
-    }
+    options.MultipartBodyLengthLimit = 10L * 1024 * 1024 * 1024; // 10 GB
 });
+
+// Configure Kestrel
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Limits.MaxRequestBodySize = 10L * 1024 * 1024 * 1024; // 10 GB
+});
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
 }
 
 app.UseStaticFiles();
+
 app.UseAntiforgery();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
